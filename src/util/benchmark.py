cb4ef771f9b82a92f1b35f8856438c35e7cde95e
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-12-31 20:41:32 -0300

    feat: Add a BenchData to create tables.

--- src/util/benchmark.py
+++ src/util/benchmark.py
@@ -1,27 +1,50 @@
 from time import time
+from typing import Self
+import pandas as pd
+
+from util.data import Data
 
 
 def benchmark(
-    algorithm_name: str, print_result: bool = True, save_file: str | None = None
+    algorithm_name: str, print_result: bool = True, save_csv: str | None = None
 ):
 
     def decorator(func):
 
         def wrapper(*args):
             start: float = time()
-            func(*args)
+            data: Data = func(*args)
             finish: float = time() - start
 
             if print_result:
                 print(f"Total time of {algorithm_name}: {finish}s")
 
             # to save the results
-            if save_file is not None:
-                with open(save_file, "a") as file:
-                    file.write(str(finish) + "\n")
+            if save_csv is not None:
+                swaps, comparisons = data.get_swaps_comparisons()
+                with open(save_csv, "a") as file:
+                    file.write(
+                        f"{data.get_order().name},{len(data.data)},{swaps},{comparisons},{finish}\n"
+                    )
 
             return finish
 
         return wrapper
 
     return decorator
+
+
+class BenchData:
+    def __init__(self) -> None:
+        self.df: pd.DataFrame = pd.DataFrame()
+
+    def __getattribute__(self, name: str) -> pd.Series | pd.DataFrame:
+        return self.df[name]
+
+    def read_csv(self, pathToData: str) -> Self:
+        self.df.read_csv(
+            pathToData,
+            header=None,
+            names=["Order", "Data Size", "Swaps", "Comparisons", "Time"],
+        )
+        return self
--- src/util/data.py
+++ src/util/data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Order(Enum):
-    ORDENED = 0
+    SORTED = 0
     REVERSED = 1
     RANDOM = 2
     MANY_REPETITIONS = 3
@@ -16,13 +16,18 @@ class Data:
 
     def __init__(self) -> None:
         self._data: np.ndarray | None = None
+        self._comparisons = 0
+        self._swaps: int = 0
+        self._order: Order = Order.SORTED
 
     def create_data(self, size: int, order: Order) -> None:
         if size < 1:
             raise RuntimeError("Size of data must be greater than 0")
 
+        self._order = order
+
         match order:
-            case Order.ORDENED:
+            case Order.SORTED:
                 self._data = np.arange(0, size, step=1)
             case Order.REVERSED:
                 self._data = np.arange(size - 1, -1, step=-1)
@@ -36,6 +41,18 @@ class Data:
                 self._data = np.random.randint(low=0, high=size // 100, size=size - 1)
             case _:
                 raise RuntimeError("Undefined order")
+
+    def update_comparisons(self, increment: int = 1) -> None:
+        self._comparisons += increment
+
+    def update_swaps(self, increment: int = 1) -> None:
+        self._comparisons += increment
+
+    def get_swaps_comparisons(self) -> tuple[int, int]:
+        return (self._swaps, self._comparisons)
+
+    def get_order(self) -> Order:
+        return self._order
 
     @property
     def data(self) -> np.ndarray:
